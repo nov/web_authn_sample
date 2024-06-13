a1c9f0836165f871b34a275f3301b0b43d3c1fa5
nov <nov@matake.jp>
2024-06-13 21:48:03 +0900

    try to change passkey upgrade timing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,24 +6,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    context = WebAuthn.context_for(
-      params[:client_data_json],
-      origin: request.base_url,
-      challenge: session.delete(:challenge)
-    )
-    if context.authentication?
-      authenticator = Authenticator.find_by!(credential_id: params[:credential_id])
-      context.verify!(
-        params[:authenticator_data],
-        public_cose_key: authenticator.public_cose_key,
-        sign_count: authenticator.sign_count,
-        signature: params[:signature]
-      )
-      authenticator.update(sign_count: context.sign_count)
-      authenticate authenticator.account
+    if params[:username]
+      authenticate Account.find_by!(email: params[:username])
       logged_in!
     else
-      redirect_to root_url
+      context = WebAuthn.context_for(
+        params[:client_data_json],
+        origin: request.base_url,
+        challenge: session.delete(:challenge)
+      )
+      if context.authentication?
+        authenticator = Authenticator.find_by!(credential_id: params[:credential_id])
+        context.verify!(
+          params[:authenticator_data],
+          public_cose_key: authenticator.public_cose_key,
+          sign_count: authenticator.sign_count,
+          signature: params[:signature]
+        )
+        authenticator.update(sign_count: context.sign_count)
+        authenticate authenticator.account
+        logged_in!
+      else
+        redirect_to root_url
+      end
     end
   end
 
